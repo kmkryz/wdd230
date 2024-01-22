1748c8e7ee8ea59d7cad00aec1efac184b70071c
kmkryz <kmkryz@gmail.com>
2024-01-22 09:56:06 -0500

    home page update w3

--- scripts/getdates.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('currentYear').textContent = new Date().getFullYear();
-
-document.getElementById('lastModified').textContent = `Last Updated: ${document.lastModified}`;
---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+document.getElementById('lastModified').textContent = `Last Updated: ${document.lastModified}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+
