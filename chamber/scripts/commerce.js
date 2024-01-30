0c8ec4d9f8ceab04a601554bf013e4f905bfa8bd
kmkryz <kmkryz@gmail.com>
2024-01-30 15:36:18 -0500

    updated chamber site

--- chamber/scripts/commerce.js
+++ chamber/scripts/commerce.js
@@ -1,3 +1,31 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    let currentIndex = 0;
+    const slides = document.querySelectorAll('.member-slideshow .mem-slide');
+    const totalSlides = slides.length;
+
+    function showSlide(index) {
+        // Hide all slides
+        slides.forEach(slide => slide.style.display = 'none');
+
+        // Show the specified slide
+        slides[index].style.display = 'flex';
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % totalSlides;
+        showSlide(currentIndex);
+    }
+
+    // Initially show the first slide
+    showSlide(currentIndex);
+
+    // Change slide every 3 seconds
+    setInterval(nextSlide, 10000);
+});
+
+
+
 document.getElementById('currentYear').textContent = new Date().getFullYear();
 
 document.getElementById('lastModified').textContent = `Last Updated: ${document.lastModified}`;
@@ -30,29 +58,3 @@ function showSlides() {
 
 
 
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    let currentIndex = 0;
-    const slides = document.querySelectorAll('.member-slideshow .mem-slide');
-    const totalSlides = slides.length;
-
-    function showSlide(index) {
-        // Hide all slides
-        slides.forEach(slide => slide.style.display = 'none');
-
-        // Show the specified slide
-        slides[index].style.display = 'flex';
-    }
-
-    function nextSlide() {
-        currentIndex = (currentIndex + 1) % totalSlides;
-        showSlide(currentIndex);
-    }
-
-    // Initially show the first slide
-    showSlide(currentIndex);
-
-    // Change slide every 3 seconds
-    setInterval(nextSlide, 10000);
-});
