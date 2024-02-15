1acd09e05f5201e23e4ef72642d3c90849967e0d
kmkryz <kmkryz@gmail.com>
2024-02-15 10:11:12 -0500

    Site visits local storage

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,19 @@ document.getElementById('lastModified').textContent = `Last Updated: ${document.
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
+const visitsDisplay = document.querySelector('#visits')
+
+let numVisits = Number(window.localStorage.getItem('numVisits-ls')) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit! 😱`;
+}
+
+numVisits++;
+
+localStorage.setItem('numVisits-ls', numVisits);
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
