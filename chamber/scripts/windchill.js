003c116d74493ba88e31b4905000df14248717f2
kmkryz <kmkryz@gmail.com>
2024-03-16 11:12:02 -0400

    forecast

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -17,7 +17,7 @@ function calculateWindChill(temperature, windSpeed) {
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=41.24&lon=-73.2&units=imperial&appid=d354d22d1fdc9585f60e31bb1de88c80';
+const url = 'https://api.openweathermap.org/data/2.5/forecast?lat=41.24&lon=-73.2&units=imperial&appid=d354d22d1fdc9585f60e31bb1de88c80';
 
 
 
