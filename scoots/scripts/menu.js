ec57185f09cd59b424b5065e04d44f77b65eb191
kmkryz <kmkryz@gmail.com>
2024-04-02 15:29:05 -0400

    final proj 4.2

---
+++ scoots/scripts/menu.js
@@ -0,0 +1,11 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.getElementById('menu');
+    const nav = document.querySelector('nav');
+
+    menuButton.addEventListener('click', function() {
+        nav.classList.toggle('active');
+    });
+});
+
